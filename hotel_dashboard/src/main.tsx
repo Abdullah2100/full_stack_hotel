ee7f5840da2d5e6fc9d6fc3d75bab037b8f6bf8e
ali50001 <ali771946613@gmail.com>
2024-12-14 14:27:11 +0300

    feat: adding  notfound page

--- hotel_dashboard/src/main.tsx
+++ hotel_dashboard/src/main.tsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import NotFoundPage from './pages/NotFound/notfound'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='*'  element={<NotFoundPage/>} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
