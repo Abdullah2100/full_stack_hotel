c19a61593ba2563fe794345e22544971bcbe3ee6
ali50001 <ali771946613@gmail.com>
2025-03-08 16:07:33 +0300

    ddd

---
+++ hotel_mobile/app/src/main/java/com/example/hotel_mobile/Modle/Screens.kt
@@ -0,0 +1,21 @@
+package com.example.hotel_mobile.Modle
+
+import kotlinx.serialization.Serializable
+
+ object Screens {
+
+    @Serializable
+    object authGraph
+
+    @Serializable
+    object login
+
+    @Serializable
+    object signUp
+
+    @Serializable
+    object homeGraph
+
+    @Serializable
+    object home
+}
